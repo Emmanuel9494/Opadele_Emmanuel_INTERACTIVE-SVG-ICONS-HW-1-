a6e022f67a54675ba14ecde327c2b565d9df1139
Emmanuel9494 <144351016+Emmanuel9494@users.noreply.github.com>
2024-01-25 07:43:05 -0500

    All Script
    After using the console in the browser to confirm, All the required scripts for the SVG's are in and working properly.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Duck svg js
 console.log("JS file connected");
 
 const vectorGraphic = document.querySelector('#duck-main');
@@ -8,3 +9,48 @@ function logThisId() {
 }
 
 vectorGraphic.addEventListener('click', logThisId);
+
+
+
+//clock svg js
+
+
+const ClockMotion = document.querySelector('#clock');
+
+function logClockId() {
+    console.log(clock);
+    console.log('When you need to check time, pull down this and you have a', clock.id);
+}
+
+ClockMotion.addEventListener('click', logClockId);
+
+
+//lamp-dance svg js
+const Dance = document.querySelector('#lamp-dance');
+
+function logLampLightId() {
+    console.log(Dance);
+    console.log('Ther is a man, There is a pole, it is a ', Dance.id);
+}
+
+Dance.addEventListener('click', logLampLightId);
+
+//pac-man svg js
+const PacMan = document.querySelector('#pac-man');
+
+function logBigPacId() {
+    console.log(PacMan);
+    console.log('Without the sound, it is still', PacMan.id);
+}
+
+PacMan.addEventListener('mouseover', logBigPacId);
+
+//world-car svg js
+const Travel = document.querySelector('#world-car');
+
+function logWorldCarId() {
+    console.log(Travel);
+    console.log('Pack your bags, say your byes and lets bounce in this', Travel.id);
+}
+
+Travel.addEventListener('mouseout', logWorldCarId);
